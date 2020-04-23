d9faf6089f8bc8a1a735c21ff577fc3154afae07
Philippe Patti <43197@etu.he2b.be>
2020-04-23 14:07:41 +0200

    Fix constexpr

--- 02 Rainbow/src/util/rt-utils.hpp
+++ 02 Rainbow/src/util/rt-utils.hpp
@@ -13,7 +13,7 @@
 namespace be::esi::secl::pn
 {
 
-constexpr unsigned PWD_SIZE = 5;                                                                                                                                                                                               /**< The minimal password size */
+inline unsigned PWD_SIZE = 5;                                                                                                                                                                                               /**< The minimal password size */
 constexpr unsigned SIZE_AZ_O9 = 36;                                                                                                                                                                                      /**< Number of valid caracters for a password */
 constexpr char AZ_O9[SIZE_AZ_O9] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; /**< All valid char for a password */
 
